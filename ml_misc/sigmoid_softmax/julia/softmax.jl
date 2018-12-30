0f4b22b8b1eb3432266b829bb2bcfe899f6a507f
Joshua Bowles <jbowayles@gmail.com>
2018-12-30 11:33:20 -0700

    softmax updates

--- ml_misc/sigmoid_softmax/julia/softmax.jl
+++ ml_misc/sigmoid_softmax/julia/softmax.jl
@@ -1,20 +1,102 @@
-using LinearAlgebra
-scores = [
+#abstract exponentiation function, subtract max for numerical stability
+_exp(x::AbstractVecOrMat) = exp.(x .- maximum(x))
+#abstract exponentiation function, subtract max for numerical stability and scale by theta
+_exp(x::AbstractVecOrMat, θ::AbstractFloat) =   exp.((x .- maximum(x)) * θ)
+#softmax algorithm expects stablized eponentiated e
+_sftmax(e::AbstractVecOrMat, d::Integer) = (e ./ sum(e, dims = d))
+
+# top level softmax function
+function softmax(X::AbstractVecOrMat{T}, dim::Integer)::AbstractVecOrMat where T <: AbstractFloat
+    _sftmax(_exp(X), dim)
+end
+
+function softmax(X::AbstractVecOrMat{T}, dim::Integer, θ::Float64)::AbstractVecOrMat where T <: AbstractFloat
+    _sftmax(_exp(X, θ), dim)
+end
+
+
+
+scores4 = [1.1, 5.0, 2.8, 7.3] # won't work: 4-element Array{Float64,1}
+scores1x4 = [1.1 5.0 2.8 7.3] # 1×4 Array{Float64,2} ... need to transpose
+scores4x4 = [
     1.0 2.0 3.0 6.0;
     2.0 4.0 5.0 6.0;
     3.0 8.0 7.0 6.0;
     3.0 8.0 7.0 6.0;
 ]
+scores3x4 = [
+    1.1 5.0 2.2 7.3;
+    6.5 3.2 8.8 5.3;
+    2.7 5.1 9.6 7.4;
+]
+X =[
+    1.1 5.0 2.2 7.3;
+    6.5 3.2 8.8 5.3;
+    2.7 5.1 9.6 7.4;
+]
 
-function softmax_naive(x)
-    exp.(x) ./ sum(exp.(x), dims=1)
+Z =[
+    [1.1 5.0 2.2 7.3];
+    [6.5 3.2 8.8 5.3];
+    [2.7 5.1 9.6 7.4];
+]
+
+Y = cat(
+    [0.844  0.237; 0.364  0.768; 0.811  0.959;],
+    [0.511  0.06; 0.594  0.029; 0.963  0.292;],
+    [0.463  0.869; 0.704  0.786; 0.173  0.89;], dims=3)
+
+
+# softmax over rows
+softmax(X, 1, 0.5)
+#3×4 Array{Float64,2}
+# 0.0552325  0.406861  0.0145858  0.413368
+# 0.821845   0.165417  0.395459   0.15207
+# 0.122922   0.427721  0.589955   0.434562
+
+# softmax over columns
+softmax(X, 2, 0.5)
+#3×4 Array{Float64,2}:
+# 0.0312892  0.219922   0.0542321  0.694557
+# 0.204121   0.0392014  0.644654   0.112024
+# 0.0215954  0.0716994  0.680265   0.22644
+
+# softmax over columns, minimze!
+softmax(X, 2, 50.0)
+#3×4 Array{Float64,2}:
+# 2.33728e-135  1.13798e-50   1.79849e-111  1.0
+# 1.13798e-50   2.49773e-122  1.0           9.96473e-77
+# 1.47368e-150  1.92195e-98   1.0           1.68891e-48
+
+
+
+#chkdim(x::AbstractVecOrMat) = size(x, 1) == 1 ? cat(dims = 1, x, ones(size(x))) : x
+smax_naive(x, dims=1, θ=1.0) = exp.(x*θ) ./ sum(exp.(x*θ), dims = d)
+f(x) = exp.(x) ./ sum(exp.(x))
+
+#1xY matrix (unit vector) needs to sum across dimension 2
+function softmax_naive(x::Matrix)::Matrix
+    r, _ = size(x)
+    if r == 1
+        return exp.(x) ./ sum(exp.(x), dims = 2)
+    end
+    exp.(x) ./ sum(exp.(x), dims = 1)
 end
 
-function softmax_wrong(x)
-    exp.(x) / sum(exp.(x), dims=1)
+#1xY matrix (unit vector) needs to sum across dimension 2
+function softmax_naive(x::Matrix, θ::Float64)::Matrix
+    r, _ = size(x)
+    if r == 1 
+        return exp.(x * θ) ./ sum(exp.(x * θ), dims = 2)
+    end
+    exp.(x * θ) ./ sum(exp.(x * θ), dims = 1)
 end
 
-function ∇softmax!(out::AbstractVecOrMat, Δ::AbstractVecOrMat, xs::AbstractVecOrMat)
-  s = sum(exp, xs, dims=1)
-  out .= exp.(xs)./s.*(Δ .- sum(Δ .* exp.(xs), dims=1)./s)
+#== fix types to work with n-dims....
+_aexp(x, θ::AbstractFloat) =   exp.((x .- maximum(x)) * θ)
+#softmax algorithm expects eponentiated e
+_sftmax(e, d::Integer) = (e ./ sum(e, dims = d))
+function softmax(X, dim::Integer, θ::Float64)
+    _sftmax(_aexp(X, θ), dim)
 end
+==#
--- ml_misc/sigmoid_softmax/python/softmax.py
+++ ml_misc/sigmoid_softmax/python/softmax.py
@@ -5,6 +5,87 @@
 import numpy as np
 
 
+def softmax(X, theta=1.0, axis=None):
+    """
+    Compute softmax for each elem along axis
+
+    params
+    ------
+    X: ND-Array, floats
+    theta: float; used to multiply prio exponentiation
+    axis: axis to comput values; defaults to first non-singleton axis
+    """
+    # at least 2D
+    y = np.atleast_2d(X)
+    if axis is None:
+        axis = next(j[0] for j in enumerate(y.shape) if j[1] > 1)
+    # scale by theta parameter
+    y = y * float(theta)
+    # sub max for numerical stability
+    y = y - np.expand_dims(np.max(y, axis=axis), axis)
+    # exponentiate
+    y = np.exp(y)
+    # take sum along axis
+    ax_sum = np.expand_dims(np.sum(y, axis=axis), axis)
+    # divide elem-wise
+    p = y / ax_sum
+    # if Xas 1D flatten back
+    if len(X.shape) == 1:
+        p = p.flatten()
+    return p
+
+
+scores4 = np.array([1.1, 5.0, 2.8, 7.3])
+scores4x4 = np.array([
+    [1.0, 2.0, 3.0, 6.0],
+    [2.0, 4.0, 5.0, 6.0],
+    [3.0, 8.0, 7.0, 6.0],
+    [3.0, 8.0, 7.0, 6.0],
+])
+scores3x4 = np.array([
+    [1.1, 5.0, 2.2, 7.3],
+    [6.5, 3.2, 8.8, 5.3],
+    [2.7, 5.1, 9.6, 7.4],
+])
+
+
+X = np.array([
+    [1.1, 5.0, 2.2, 7.3],
+    [6.5, 3.2, 8.8, 5.3],
+    [2.7, 5.1, 9.6, 7.4],
+])
+
+# softmax over rows
+softmax(X, theta=0.5, axis=0)
+#array([[0.05523253, 0.40686118, 0.01458578, 0.41336824],
+#       [0.82184522, 0.16541741, 0.39545889, 0.15206968],
+#       [0.12292225, 0.4277214 , 0.58995534, 0.43456208]])
+
+# softmax over columns
+softmax(X, theta=0.5, axis=1)
+# array([[0.03128922, 0.21992213, 0.05423213, 0.69455652],
+#       [0.20412102, 0.03920142, 0.64465356, 0.11202399],
+#       [0.02159544, 0.07169938, 0.68026473, 0.22644046]])
+
+# softmax over columns, and squash it!
+softmax(X, theta=50.0, axis=1)
+# array([[2.33727929e-135, 1.13797987e-050, 1.79848622e-111,
+#        1.00000000e+000],
+#       [1.13797987e-050, 2.49772757e-122, 1.00000000e+000,
+#        9.96473301e-077],
+#       [1.47368188e-150, 1.92194773e-098, 1.00000000e+000,
+#        1.68891188e-048]])
+
+
+
+
 def softmax_naive(x):
-    """ naive version of softmax does not scale the ... """
+    """ naive version of softmax, not theta scale... no numerical stability"""
     return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+
+def softmax_theta(x, theta=1.0):
+    """ naive version of softmax... no numerical stability"""
+    t = np.exp(x * theta)
+    t /= np.sum(t, axis=0)
+    return t
