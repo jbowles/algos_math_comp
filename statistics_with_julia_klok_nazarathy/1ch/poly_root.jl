fa8dba21c342988582e2ac64775f3f438d9ff26b
Joshua Bowles <jbowayles@gmail.com>
2019-09-04 19:33:22 -0600

    stats with julia, remove smit-waterman rust

---
+++ statistics_with_julia_klok_nazarathy/1ch/poly_root.jl
@@ -0,0 +1,18 @@
+using Roots
+
+#==
+f(x)=−10x2 +3x+1
+-----------------
+x= −3 ± sqrt(2^2−4(−10)) /2(-10)= 3±7 /20   ⇒ x1 =0.5, x2 =−0.2.
+==#
+function polyGen(a...)
+    n = length(a) - 1
+    poly = function(x)
+        return sum([a[i + 1] * x^i for i in 0:n])
+    end
+    return poly
+end
+
+polynom = polyGen(1, 3, -10)
+zeroVals = Roots.find_zeros(polynom, -10, 10)
+println("zeros of function f(x): ", zeroVals)
